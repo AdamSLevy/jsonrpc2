63b1396665b5d17a34573d082725baf3be670a14
Adam S Levy <adam@aslevy.com>
2018-07-03 10:34:23 -0800

    docs(overview): Add code snippets to overview

--- doc.go
+++ doc.go
@@ -15,16 +15,27 @@
 //
 // Clients can use the Request, Response, and Error types with the json and
 // http packages to make HTTP JSON-RPC 2.0 calls and parse their responses.
+//      reqBytes, _ := json.Marshal(jsonrpc2.NewRequest("subtract", 0, []int{5, 1}))
+//      httpResp, _ := http.Post("www.example.com", "application/json",
+//              bytes.NewReader(reqBytes))
+//      respBytes, _ := ioutil.ReadAll(httpResp.Body)
+//      response := jsonrpc2.Response{}
+//      json.Unmarshal(respBytes, &response)
 //
 // Server
 //
 // Servers must implement their RPC method functions to match the MethodFunc
-// type and then register their function with a name using RegisterMethod(name,
-// function). Read the documentation for RegisterMethod and MethodFunc for more
-// information. RemarshalJSON is a convenience function for converting the
-// abstract params argument into a custom concrete type.
+// type.
+//      type MethodFunc func(params interface{}) Response
+// Methods must be registered with a name using RegisterMethod().
+//      jsonrpc2.RegisterMethod("subtract", mySubtractMethodFunc)
+// Read the documentation for RegisterMethod and MethodFunc for more
+// information.
 //
-// After all methods are registered set up an HTTP Server with
+// For convenience, methods can use RemarshalJSON() for converting the abstract
+// params argument into a custom concrete type.
+//
+// After all methods are registered, set up an HTTP Server with
 // HTTPRequestHandler as the handler.
 //      http.ListenAndServe(":8080", jsonrpc2.HTTPRequestHandler)
 package jsonrpc2
