78db6fb1562d36a0ab8421bcfca1f6c53a8c994e
Adam S Levy <adam@aslevy.com>
2018-07-12 22:49:52 -0800

    docs(version): Bump version number, update README

--- example_test.go
+++ example_test.go
@@ -1,4 +1,4 @@
-// github.com/AdamSLevy/jsonrpc2 v1.1.1
+// github.com/AdamSLevy/jsonrpc2 v2.0.0
 // Copyright 2018 Adam S Levy. All rights reserved.
 // Use of this source code is governed by the MIT license that can be found in
 // the LICENSE file.
