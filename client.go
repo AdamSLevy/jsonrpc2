1429ace8058f48f5b63ac0d84cd0e8cd237eff2b
Adam S Levy <adam@aslevy.com>
2020-02-17 14:34:02 -0900

    Don't fail immediately on bad HTTP status code
    Try to parse the http.Response.Body as a Response regardless of
    http.Response.StatusCode since some jsonrpc2 servers use status code AND
    jsonrpc2.Error to indicate a method error. This is wrong but common.
    
    Also, add ErrorUnexpectedHTTPResponse to allow deeper inspection of the
    http.Response if parsing into a Response fails.
    
    This slightly changes the error handling behavior for the
    Client.Request().

--- client.go
+++ client.go
@@ -39,6 +39,29 @@ type Logger interface {
 	Printf(string, ...interface{})
 }
 
+func newErrorUnexpectedHTTPResponse(err error, body []byte, res *http.Response) error {
+	return ErrorUnexpectedHTTPResponse{err, body, res}
+}
+
+// ErrorUnexpectedHTTPResponse wraps errors that occur during unmarshling of
+// the http.Response.Body into a Response, along with the full bytes of the
+// http.Response.Body and the http.Response itself.
+type ErrorUnexpectedHTTPResponse struct {
+	UnmarshlingErr error
+	Body           []byte
+	*http.Response
+}
+
+// Error returns err.UnmarshalingErr.Error().
+func (err ErrorUnexpectedHTTPResponse) Error() string {
+	return err.UnmarshlingErr.Error()
+}
+
+// Unwrap return err.UnmarshalingErr.
+func (err *ErrorUnexpectedHTTPResponse) Unwrap() error {
+	return err.UnmarshlingErr
+}
+
 // Client embeds http.Client and provides a convenient way to make JSON-RPC 2.0
 // requests.
 type Client struct {
@@ -58,9 +81,15 @@ type Client struct {
 //
 // If ctx is not nil, it is added to the http.Request.
 //
-// If an Error Response is received, then an Error type is returned. Other
-// potential errors can result from json.Marshal and params, json.Unmarshal and
-// result, http.NewRequest and url, or network errors from c.Do.
+// If the http.Response is received without error, but cannot be parsed into a
+// Response, then an ErrorUnexpectedHTTPResponse is returned containing the
+// Unmarshaling error, the raw bytes of the http.Response.Body, and the
+// http.Response.
+//
+// If the Response.HasError() is true, then the Error is returned.
+//
+// Other potential errors can result from json.Marshal and params,
+// http.NewRequest and url, or network errors from c.Do.
 //
 // A pseudorandom uint between 1 and 5000 is used for the Request.ID.
 //
@@ -75,72 +104,66 @@ type Client struct {
 // c.Log. If c.Log == nil, then c.Log = log.New(os.Stderr, "", 0).
 func (c *Client) Request(ctx context.Context, url, method string,
 	params, result interface{}) error {
-	// Generate a random ID for this request.
+
+	// Generate a psuedo random ID for this request.
 	reqID := rand.Int()%5000 + 1
 
 	// Marshal the JSON RPC Request.
-	reqJrpc := Request{ID: reqID, Method: method, Params: params}
+	req := Request{ID: reqID, Method: method, Params: params}
 	if c.DebugRequest {
 		if c.Log == nil {
 			c.Log = log.New(os.Stderr, "", 0)
 		}
-		c.Log.Println(reqJrpc)
+		c.Log.Println(req)
 	}
-	reqBytes, err := reqJrpc.MarshalJSON()
+	reqData, err := req.MarshalJSON()
 	if err != nil {
 		return err
 	}
 
 	// Compose the HTTP request.
-	reqHTTP, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(reqBytes))
+	httpReq, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(reqData))
 	if err != nil {
 		return err
 	}
 	if ctx != nil {
-		reqHTTP = reqHTTP.WithContext(ctx)
+		httpReq = httpReq.WithContext(ctx)
 	}
-	reqHTTP.Header.Add(http.CanonicalHeaderKey("Content-Type"), "application/json")
+	httpReq.Header.Add(http.CanonicalHeaderKey("Content-Type"), "application/json")
 	for k, v := range c.Header {
-		reqHTTP.Header[http.CanonicalHeaderKey(k)] = v
+		httpReq.Header[http.CanonicalHeaderKey(k)] = v
 	}
 	if c.BasicAuth {
-		reqHTTP.SetBasicAuth(c.User, c.Password)
+		httpReq.SetBasicAuth(c.User, c.Password)
 	}
 
 	// Make the request.
-	res, err := c.Do(reqHTTP)
+	httpRes, err := c.Do(httpReq)
 	if err != nil {
 		return err
 	}
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusBadRequest {
-		return fmt.Errorf("http: %v", res.Status)
-	}
+	defer httpRes.Body.Close()
 
 	// Read the HTTP response.
-	resBytes, err := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(httpRes.Body)
 	if err != nil {
 		return err
+	}
+	if c.DebugRequest {
+		fmt.Println("<--", string(body))
+		fmt.Println()
 	}
 
 	// Unmarshal the HTTP response into a JSON RPC response.
 	var resID int
-	resJrpc := Response{Result: result, ID: &resID}
-	if err := json.Unmarshal(resBytes, &resJrpc); err != nil {
-		return err
+	res := Response{Result: result, ID: &resID}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return newErrorUnexpectedHTTPResponse(err, body, httpRes)
 	}
-	if c.DebugRequest {
-		if resJrpc.HasError() {
-			resJrpc.Result = nil
-		}
-		fmt.Println("<--", string(resBytes))
-		fmt.Println()
+
+	if res.HasError() {
+		return res.Error
 	}
-	if resJrpc.HasError() {
-		return resJrpc.Error
-	}
-	if resID != reqID {
-		return fmt.Errorf("request/response ID mismatch")
-	}
+
 	return nil
 }
